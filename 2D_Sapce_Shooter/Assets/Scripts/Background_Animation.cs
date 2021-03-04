b7b7fc8709ef52eec772e97c080eba26aacc35f7
Priyanka Wakchaure <priyankawakchaure89@gmail.com>
2021-03-04 09:03:52 +0530

    Updated UI

---
+++ 2D_Sapce_Shooter/Assets/Scripts/Background_Animation.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Background_Animation : MonoBehaviour
+{
+    public float bgScroolingSpeed = 5;
+    Material material;
+    Vector2 offset;
+
+    private void Start()
+    {
+        material = GetComponent<Renderer>().material;
+        offset = new Vector2(0.0f, bgScroolingSpeed);
+    }
+
+    private void Update()
+    {
+        material.mainTextureOffset += offset * Time.deltaTime;
+    }
+}
