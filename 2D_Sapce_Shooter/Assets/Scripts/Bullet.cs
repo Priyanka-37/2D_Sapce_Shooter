2fbf95526d8a7c46caefd4490d3457bc2a7005a0
Sonu Shivhare <sonushivhare89@gmail.com>
2021-04-28 23:38:10 +0530

    Updated

---
+++ 2D_Sapce_Shooter/Assets/Scripts/Bullet.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public GameObject enemyExplosion;
+    public GameObject playerExplosion;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            Instantiate(playerExplosion, other.transform.position, other.transform.rotation);
+            PlayerPrefs.SetInt("Score", GameManager.instance.scoreManager.score);
+            Destroy(this.gameObject);
+            Destroy(other.gameObject);
+        }
+
+        if (other.tag == "Projectile")
+        {
+            Instantiate(enemyExplosion, transform.position, transform.rotation);
+            Destroy(this.gameObject);
+            Destroy(other.gameObject);
+        }
+    }
+}
